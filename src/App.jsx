fff4392be28f8ec9f8fea1d66a08a05764e44704
punitsdh <punitsdh@gmail.com>
2025-07-05 23:33:35 +0530

    connections and requests

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Login from "./components/Login";
 import Feed from "./components/Feed";
 import appStore from "./utils/appStore";
 import { Provider } from "react-redux";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
               <Route path="/" element = {<Feed />}/>
               <Route path = "/login" element = {<Login />} />
               <Route path = "/profile" element = {<Profile />} />
+              <Route path = "/connections" element = {<Connections />} />
+              <Route path = "/requests" element = {<Requests />} />
             </ Route>
           </Routes>
         </BrowserRouter>
---
+++ src/components/Connections.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { BASE_URL } from '../utils/constants';
+import {addConnections} from "../utils/connectionSlice";
+import axios from 'axios';
+
+
+const Connections = () => {
+  const connections = useSelector((store)=> store.connections);
+  const dispatch = useDispatch();
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true,
+      });
+      dispatch(addConnections(res.data.data));
+    } catch (err) {
+      res.status(400).send('Bad Request');
+      console.error(err);
+    }
+  }
+
+  useEffect(()=> {
+    fetchConnections();
+  }, []);
+
+  if(!connections) return;
+
+  if(connections.length === 0) return <h1>No Connections Found</h1>;
+
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-white text-3xl">Connections</h1>
+
+      {connections.map((connection) => {
+        const { _id, firstName, lastName, imageUrl, age, gender, about } =
+          connection;
+
+        return (
+          <div
+            key={_id}
+            className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+          >
+            <div>
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full object-cover"
+                src={imageUrl}
+              />
+            </div>
+            <div className="text-left mx-4 ">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{about}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Connections
